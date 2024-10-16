1e3696b4b1c9f4cd6c17b1a4d182689480e3f5f1
sverma507 <sverma70568@gmail.com>
2024-10-16 09:56:10 +0530

    new

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <img src={require("./Images/nexa-login.jpg")}/>
+      </div>
+      <div className='border-2 border-black'></div>
+    </div>
+  )
+}
+
+export default Login
